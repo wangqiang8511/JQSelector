619f70ae082f99bff631f1ac90e34620fc4834dc
Wang Qiang <wangqiang8511@gmail.com>
2011-09-08 21:37:49 +0800

    init file

---
+++ __init__.py
@@ -0,0 +1,46 @@
+#
+# BSD License
+# Copyright (c) 2011, Wang Qiang
+# All rights reserved.
+
+"""
+JQSelector
+
+Process HTML file and XML file.
+
+Function list:
+
+Three basic parser:
+elements = parseByElement(html, elementName)
+elements = parseByTagProperties(html, tagName, **properties)
+elements = parseByProperties(html, **properties)
+
+Advanced Selector:
+elements = selectByClass(html, classname)
+elements = selectById(html, id)
+
+JQuery-like Selector:
+elements = JQSelect(html, selectStr)
+
+selectStr specification
+'tag.class#id[name="value"]'
+[name|="value"]
+[name*="value"]
+[name~="value"]
+[name!="value"]
+[name^="value"]
+[name$="value"]
+'str1, str2,...'
+'str1 > str2'
+'str1 + str2'
+'str1 ~ str2'
+'str1 ~ str2 > str3...'
+"""
+
+__version__ = '1.0'
+__author__ = 'Wang Qiang'
+__license__ = 'BSD License'
+
+__all__ = ['JQSelector']
+
+from JQSelector.JQSelector import JQSelect 
